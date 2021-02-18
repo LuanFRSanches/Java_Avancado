1d63bffeb7461bc54395ced36a0237a552d84013
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-17 21:34:13 -0400

    InnerClassStatic

---
+++ InnerClassStatic/src/main/java/br/com/softblue/java/inner/ClasseDeFora.java
@@ -0,0 +1,12 @@
+package br.com.softblue.java.inner;
+
+public class ClasseDeFora {
+
+	public static class ClasseDeDentro {
+		private int x = 10;
+
+		public void imprimir() {
+			System.out.println(x);
+		}
+	}
+}
---
+++ InnerClassStatic/src/main/java/br/com/softblue/java/inner/Main.java
@@ -0,0 +1,7 @@
+package br.com.softblue.java.inner;
+
+public class Main {
+	public static void main(String[] args) {
+		ClasseDeFora.ClasseDeDentro c = new ClasseDeFora.ClasseDeDentro();
+	}
+}
