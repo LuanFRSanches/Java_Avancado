6ba058f1c1e6866667fea9ad049f71b4c7a943fa
LuanFRSanches <luanfelipersanches@gmail.com>
2021-03-04 22:49:22 -0400

    Formulario em JavaFX

--- JavaFXUI1/src/main/java/br/com/sofblue/java/javafx/JavaFXApplication.java
+++ JavaFXUI1/src/main/java/br/com/sofblue/java/javafx/JavaFXApplication.java
@@ -1,13 +1,21 @@
 package br.com.sofblue.java.javafx;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
@@ -40,9 +48,35 @@ public class JavaFXApplication extends Application {
 		TextField txtEmail = new TextField();
 		GridPane.setHgrow(txtEmail, Priority.ALWAYS);
 		grid.add(txtEmail, 1, 1);
-		
-		grid.add(new Label("Sexo"), 0,2);
-	RadioButton 
+
+		grid.add(new Label("Sexo"), 0, 2);
+		RadioButton btnMasc = new RadioButton("Masculino");
+		btnMasc.setSelected(true);
+		RadioButton btnFem = new RadioButton("Feminino");
+
+		ToggleGroup group = new ToggleGroup();
+		btnMasc.setToggleGroup(group);
+		btnFem.setToggleGroup(group);
+		HBox radioBox = new HBox(5.0, btnMasc, btnFem);
+		grid.add(radioBox, 1, 2);
+
+		grid.add(new Label("Estado:"), 0, 1);
+		ChoiceBox<String> chxEstado = new ChoiceBox<String>();
+		ObservableList<String> estados = FXCollections.observableArrayList("Paraná", "São Paulo", "Mato Grosso do Sul",
+				"Amazonas");
+		chxEstado.getSelectionModel().select(0);
+		chxEstado.setItems(estados);
+		grid.add(chxEstado, 1, 3);
+
+		root.setCenter(grid);
+
+		HBox buttonBar = new HBox(5.0);
+		buttonBar.setAlignment(Pos.CENTER_RIGHT);
+		Button btnGravar = new Button("Gravar");
+		Button btnCancelar = new Button("Cancelar");
+		buttonBar.getChildren().addAll(btnGravar, btnCancelar);
+
+		root.setBottom(buttonBar);
 
 		Scene scene = new Scene(root, 300, 200);
 		primaryStage.setScene(scene);
