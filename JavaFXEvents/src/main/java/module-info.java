20587a5a137f66b5bfbe1ab6f4fa05fe6f0083de
LuanFRSanches <luanfelipersanches@gmail.com>
2021-03-05 00:25:51 -0400

    Eventos JavaFX

---
+++ JavaFXEvents/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module JavaFXEvents {
+	exports br.com.softblue.java.javafx;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+}
---
+++ JavaFXUI1/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module JavaFXUI1 {
+	exports br.com.sofblue.java.javafx;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+}
