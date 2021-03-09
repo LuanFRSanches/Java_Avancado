fc87faee47b985c3d6ee630bfe3fe80170df68ff
LuanFRSanches <luanfelipersanches@gmail.com>
2021-03-08 22:37:10 -0400

    XML e JSON

--- XMLValidateSAX/src/main/java/br/com/softblue/java/xml/Main.java
+++ XMLValidateSAX/src/main/java/br/com/softblue/java/xml/Main.java
@@ -17,28 +17,28 @@ import org.w3c.dom.NodeList;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-
+		
 		SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 		Schema schema = sf.newSchema();
 		Validator validator = schema.newValidator();
 		validator.validate(new StreamSource(new File("usuarios.xml")));
-
+		
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
 		Document doc = db.parse(new File("usuarios.xml"));
-
+		
 		Element rootTag = doc.getDocumentElement();
-
+		
 		NodeList usuarioTagList = rootTag.getElementsByTagName("usuario");
-
+		
 		for (int i = 0; i < usuarioTagList.getLength(); i++) {
 			Element usuarioTag = (Element) usuarioTagList.item(i);
-
+			
 			String id = usuarioTag.getAttribute("id");
 			String nome = usuarioTag.getElementsByTagName("nome").item(0).getTextContent();
 			String idade = usuarioTag.getElementsByTagName("idade").item(0).getTextContent();
 			String email = usuarioTag.getElementsByTagName("email").item(0).getTextContent();
-
+			
 			System.out.println(id + ", " + nome + ", " + idade + ", " + email);
 		}
 	}
